c923c787e838406cb89ab3b1214f9117cf6dccd0
manavshah <manavshah1011.ms@gmail.com>
2022-08-25 20:44:18 +0530

    Initial Commit

---
+++ typing_practice_backend/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from essential_generators import DocumentGenerator
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def ParaGraphGenerator(request):
+    gen = DocumentGenerator()
+    raw_paragraph=gen.paragraph(10,15)
+    whitelist = set('abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    idiol_para=''.join(filter(whitelist.__contains__, raw_paragraph))
+    return Response(idiol_para)    
